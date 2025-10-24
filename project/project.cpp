e6321ddbe4dbcdacf851633c3940294fb764382a
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2025-10-24 17:32:48 +0200

    Input parameters are processed, boost-interprocess added, bat file added

--- project/project.cpp
+++ project/project.cpp
@@ -3,15 +3,38 @@
 
 namespace po = boost::program_options;
 
-int main()
+int main(int argc, char* argv[])
 {
     // Declare the supported options.
     po::options_description desc("Allowed options");
     desc.add_options()
         ("help", "produce help message")
-        ("compression", po::value<int>(), "set compression level")
+        ("source", po::value<std::string>(), "set file to copy from")
+        ("destination", po::value<std::string>(), "set file to copy to")
+        ("memory", po::value<std::string>(), "name of shared memory object")
         ;
 
-    std::cout<<"Hello World!"<<std::endl;
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    if (!vm.count("source")) {
+        std::cout << "Source file was not set." << std::endl;
+    }
+    else if (!vm.count("destination")) {
+        std::cout << "Destination file was not set." << std::endl;
+    }
+    else if (!vm.count("memory")) {
+        std::cout << "Memory name was not set." << std::endl;
+	}
+    else {
+        std::cout << "ALL ARGUMENTS WERE PROVIDED (to be implemented)." << std::endl;
+    }
+
     return 0;
 }
---
+++ project/start.bat
@@ -0,0 +1,3 @@
+@echo off
+start /B build/WorkProject.exe --help
+pause
