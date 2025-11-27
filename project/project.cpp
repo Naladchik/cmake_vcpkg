73fd2c8eb0c032a9c5b46dc90639aeff3e4b2f34
Alex Viaselyeu <aliaksei.viaselyeu@gmail.com>
2025-11-27 01:23:47 +0100

    Added memory creation and removing. Two roles are resolved.

--- project/project.cpp
+++ project/project.cpp
@@ -1,7 +1,11 @@
 ﻿#include <iostream>
+#include <chrono>
+#include <thread>
 #include <boost/program_options.hpp>
+#include <boost/interprocess/shared_memory_object.hpp>
 
 namespace po = boost::program_options;
+namespace ip = boost::interprocess;
 
 int main(int argc, char* argv[])
 {
@@ -33,7 +37,45 @@ int main(int argc, char* argv[])
         std::cout << "Memory name was not set." << std::endl;
 	}
     else {
-        std::cout << "ALL ARGUMENTS WERE PROVIDED (to be implemented)." << std::endl;
+        std::cout << std::endl;
+        /*std::cout << "Source: " << vm["source"].as<std::string>() << std::endl;
+		std::cout << "Destination: " << vm["destination"].as<std::string>() << std::endl;
+		std::cout << "Memory: " << vm["memory"].as<std::string>() << std::endl;*/
+
+        // ============== MAIN LOGIC IS HERE ======================================
+
+        constexpr unsigned int CREATOR = 0;
+        constexpr unsigned int USER = 1;
+		int role = CREATOR; // 0 - creator, 1 - user
+
+        try {
+            ip::shared_memory_object shm(
+                ip::create_only,
+                vm["memory"].as<std::string>().c_str(),
+                ip::read_write
+            );
+            std::cout << "Memory object created. " << std::endl;
+        }
+        catch (const ip::interprocess_exception& ex) {
+            ip::shared_memory_object shm_obj(
+                ip::open_only,
+                vm["memory"].as<std::string>().c_str(),
+                ip::read_write
+            );
+            std::cout << "Memory object opened. " << std::endl;
+			role = 1;
+		}
+
+        if(USER == role){
+            for(int i = 0; i < 1000; ++i){
+                // Simulate work
+                //std::this_thread::sleep_for(std::chrono::microseconds(1));
+			}
+            ip::shared_memory_object::remove(vm["memory"].as<std::string>().c_str());
+            std::cout << "Memory object removed. " << std::endl;
+        }else {
+			std::cout << "User role, not removing memory object. " << std::endl;
+        }
     }
 
     return 0;
